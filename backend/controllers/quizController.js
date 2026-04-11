337b2f6842c88b2df621dcda725d75324fa3d11c
Abhiram Samala <abhiramsamala27@gmail.com>
2026-04-11 22:02:45 +0530

    Force IPv4 for mail system to fix ENETUNREACH on Railway

--- backend/controllers/quizController.js
+++ backend/controllers/quizController.js
@@ -5,11 +5,15 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
   port: 587,
-  secure: false, // Use STARTTLS
+  secure: false, 
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
-  }
+  },
+  tls: {
+    rejectUnauthorized: false
+  },
+  family: 4 // Force IPv4
 });
 
 // Verify SMTP connection on startup
